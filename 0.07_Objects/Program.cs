36899d5c6a3b1d7d7864fd44ee47aaefe7948974
Ciprian Buciumean <cnbucium@gmail.com>
2017-10-25 16:52:41 -0400

    Objects

---
+++ 0.07_Objects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Donut PaulDonut = new Donut();
+            Donut KennDonut = new Donut();
+            Donut JayDonut = new Donut();
+
+            PaulDonut.Filling = "Cherry Jelly";
+            PaulDonut.Price = 7.99M;
+            PaulDonut.Type = "Jelly";
+            PaulDonut.IsSpecial = true;
+
+            KennDonut.Filling = "Strawberry Jelly";
+            KennDonut.Price = 0;
+            KennDonut.Type = "Jelly";
+            KennDonut.IsSpecial = false;
+
+            JayDonut.Filling = "Creme";
+            JayDonut.Price = 3.50M;
+            JayDonut.Type = "AngelCreme";
+            JayDonut.IsSpecial = true;
+
+            Console.WriteLine(PaulDonut);
+            Console.WriteLine(PaulDonut.Filling);
+
+        }
+    }
+}
