1d404b7620ae6336f6cf5a0cc0301246d7c963a8
Ciprian Buciumean <cnbucium@gmail.com>
2017-10-25 14:26:40 -0400

    SwitchCase

---
+++ 0.05_SwitchCase/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._05_SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend = "Kenn";
+
+            switch (friend)
+            {
+                case "Ciprian":
+                    Console.WriteLine("Hey Ciprian.");
+                    break;
+                case "John":
+                    Console.WriteLine("John is pretty cool");
+                    break;
+                case "Kenn":
+                    Console.WriteLine("Silent master Kenn...");
+                    break;
+                default:
+                    Console.WriteLine("i don't know you.");
+                    break;
+
+            }
+        }
+    }
+}
