51060358d19c4691de949fff545e2ae21f49f189
Ciprian Buciumean <cnbucium@gmail.com>
2017-10-26 11:05:23 -0400

    Netflix Challenge

---
+++ 0.08_Objects_Challenges/Netflix.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._08_Objects_Challenges
+{
+    class Netflix
+
+    {
+        // Properties. Ignore the get; set; for now..
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Rating { get; set; }
+
+
+        //  Calling the methods on the object (invoking, executing function/methods)
+
+        public void GetSuggestion()
+        {
+            if (this.Rating > 4)
+            {
+                Console.WriteLine("This is a pretty good show.");
+            }
+
+            else
+            {
+                Console.WriteLine("Don't waste your time.");
+            }
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ 0.08_Objects_Challenges/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._08_Objects_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object Instantiation
+
+            Netflix mindHunter = new Netflix();
+            Netflix theOffice = new Netflix();
+            Netflix planetEarth = new Netflix();
+
+
+            // Set Object Values
+
+            mindHunter.Name = "MindHunter";
+            mindHunter.Genre = "Horror";
+            mindHunter.Rating = 5;
+
+            theOffice.Name = "The Office";
+            theOffice.Genre = "Comedy";
+            theOffice.Rating = 5;
+
+            planetEarth.Name = "Planet Earth";
+            planetEarth.Genre = "Documentary";
+            planetEarth.Rating = 3;
+
+            //Calling the methods
+
+
+            theOffice.GetSuggestion();
+
+            mindHunter.GetSuggestion();
+
+            planetEarth.GetSuggestion();
+
+            
+
+            //Create a list of netflix Objects// Gold Challenge
+
+            List<Netflix> favoriteShows = new List<Netflix>();
+
+            favoriteShows.Add(theOffice);
+            favoriteShows.Add(mindHunter);
+            favoriteShows.Add(planetEarth);
+
+            foreach (Netflix show in favoriteShows)
+            {
+                Console.WriteLine(show.Name);
+                Console.WriteLine(show.Genre);
+                Console.WriteLine(show.Rating);
+            }   
+            {
+
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
