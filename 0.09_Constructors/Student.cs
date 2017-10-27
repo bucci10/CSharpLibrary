ec3082f46beef6ba8727afe716fd632bd25cecba
Ciprian Buciumean <cnbucium@gmail.com>
2017-10-27 13:54:11 -0400

    Constructors

---
+++ 0.09_Constructors/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._09_Constructors
+{
+    class Student
+    {
+
+        //Constructors
+        //Constructor 1
+
+
+        public Student( string firstName, string lastName, int grade)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+        }
+
+        //Constructor 2
+        public Student(string firstName, string lastName, int age, int grade)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Grade = grade;
+        }
+
+        //Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int Grade { get; set; }
+
+
+    }
+}
