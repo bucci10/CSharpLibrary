ea5d588279bc596f8ca3e1d974a5f05c366c0f12
Ciprian Buciumean <cnbucium@gmail.com>
2017-10-30 16:47:51 -0400

    Game

---
+++ 0.10_OOP_Game/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._10_OOP_Game
+{
+    class Player: Character
+    {
+        public enum Specialization
+        {
+            HorseMange = 1,
+            TrollCat = 2,
+            KnightTemplar = 3,
+            umonnn = 4,
+            dogman = 5,
+        }
+    }
+}
---
+++ 0.10_OOP_Game/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._10_OOP_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Character myName = new Character();
+            Player newHero = new Player();
+            Enemy jenn = new Enemy();
+
+            Console.WriteLine("Hello, wonderer...\n" +
+                "What be thy name?");
+            newHero.Name = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine($"\n\n Nice to meet you, {newHero.Name}.");
+            Console.ReadLine();
+            Console.WriteLine($"Where do you come from, friend?)\n" +
+                "1:Horse Mange \n" +
+                "2:Troll Call \n:" +
+                "3:Knight Templar\n" +
+                "4:umonnn\n" +
+                "5:Dogman");
+            Player.Specialization = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
