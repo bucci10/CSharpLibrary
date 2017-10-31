378527a10bd8b733765faed0c51416939e6d6a07
Ciprian Buciumean <cnbucium@gmail.com>
2017-10-31 10:23:22 -0400

    BuildAlongGame

---
+++ 0.11_OOP_Game_BulidAlong/Character.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._11_OOP_Game_BulidAlong
+{
+    public abstract class Character
+    {
+        //Constants 
+        const int MIN_HEALTH = 0;
+
+        //Properties 
+        public string Name { get; set; }
+        public bool IsAlive { get; set; }
+        public int Level { get; set; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+        public int Energy { get; set; }
+        public double CritChance { get; set; }
+
+        //Methods
+        public double Attack()
+        {
+            Random rnd = new Random();
+            int spread = rnd.Next(-5, 6);
+            int damage = this.AttackPower + spread;
+            bool isCrit = IsCritical();
+            return 0;
+        }
+
+        public bool IsCritical()
+        {
+            return true;
+            
+
+        }
+
+    }   
+
+}
---
+++ 0.11_OOP_Game_BulidAlong/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._11_OOP_Game_BulidAlong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
