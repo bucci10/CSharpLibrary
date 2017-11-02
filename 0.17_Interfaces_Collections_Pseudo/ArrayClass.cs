0678ba85898b683d02098acbbbdcf96cb7415fcf
Ciprian Buciumean <cnbucium@gmail.com>
2017-11-02 13:42:41 -0400

    Initial Interfaces

---
+++ 0.17_Interfaces_Collections_Pseudo/ArrayClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._17_Interfaces_Collections_Pseudo
+{
+    class ArrayClass : IEnum, IList, ICollect
+    {      // Implementing Interfaces, Only interfaces will allow for multiple inheritance 
+
+        public void AddStuff()
+        {
+            Console.WriteLine("Added stuff to the array...");
+        }
+
+        public void CheckTheCapacity()
+        {
+            Console.WriteLine("You can't check the capacity of an array...");
+        }
+
+        public void CheckTheCount()
+        {
+            Console.WriteLine("Checking the count of the array...");
+        }
+
+        public void IterateOverCollection()
+        {
+            Console.WriteLine("Iterating over the array...");
+        }
+
+        public void RemoveStuff()
+        {
+            Console.WriteLine("Remove stuff from the array...");
+        }
+    }
+}
