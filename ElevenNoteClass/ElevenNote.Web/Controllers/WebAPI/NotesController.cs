1a25573a882ce3bfa7458fbf33ce0f7f17142cc6
Ciprian <cnbucium@gmail.com>
2017-11-17 12:59:06 -0500

    Setup Web API for Starring Notes

--- ElevenNoteClass/ElevenNote.Web/Controllers/WebAPI/NotesController.cs
+++ ElevenNoteClass/ElevenNote.Web/Controllers/WebAPI/NotesController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,5 +14,34 @@ namespace ElevenNote.Web.Controllers.WebAPI
     [RoutePrefix("api/Notes")]
     public class NotesController : ApiController
     {
+        private bool SetStarState(int noteId, bool newState)
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+
+            var detail = service.GetNoteById(noteId);
+
+            var updateNote =
+                new NoteEditModel
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content,
+                    IsStarred = newState
+                };
+
+            return service.UpdateNote(updateNote);
+        }
+
+        [Route("{id}/Star")]
+        public bool Put(int id)
+        {
+            return SetStarState(id, true);
+        }
+        [Route("{id}/Star")]
+        public bool Delete(int id)
+        {
+            return SetStarState(id, false);
+        }
     }
 }
